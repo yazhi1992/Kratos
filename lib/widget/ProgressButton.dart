a5cf8cb282ffd1007d3c50c85b1324605b8dba64
zengyazhi <zengyazhiwork@163.com>
2018-08-02 12:51:53 +0800

    新增仿应用宝下载应用按钮

--- lib/widget/ProgressButton.dart
+++ lib/widget/ProgressButton.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 
+//仿应用宝下载应用按钮
 class ProgressButton extends StatelessWidget {
 
-  var backgroundColor;
-  var textColor;
+  var backgroundColor; //进度为0时的背景颜色
+  var textColor; //进度为0是的文字颜色
   var width;
   var height;
   var fontSize;
   var text;
-  var progress;
+  var progress; //0~1 进度值
+  var radius;
+  var boardWidth;
 
-  ProgressButton(
-      {@required this.backgroundColor, @required this.textColor, @required this.width, @required this.height,
-        this.fontSize = 15.0,
-        @required this.progress,
-        @required this.text});
+  ProgressButton({
+    @required this.width,
+    @required this.height,
+    @required this.progress,
+    @required this.text,
+    this.backgroundColor = Colors.white,
+    this.textColor = Colors.blue,
+    this.fontSize = 13.0,
+    this.radius = 5.0,
+    this.boardWidth = 1.0,}) : assert(progress > 0 && progress <= 1);
 
   @override
   Widget build(BuildContext context) {
@@ -29,43 +37,22 @@ class ProgressButton extends StatelessWidget {
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: fontSize, color: textColor)),),
           ),
-          Stack(
-              children: <Widget>[
-                ClipRect(
-                  clipper: _RatingBarClipper(width: width * progress),
-                  child: Container(width: width,
-                      height: height,
-                      child: CustomPaint(
-                          painter: MyPainter(
-                            textColor,
-                            backgroundColor,
-                            text,
-                            progress,
-                            fontSize,
-                          ),
-                          size: Size(width, height)
-                      )),
-                )
-              ]),
+          Container(width: width,
+            height: height,
+            child: CustomPaint(
+                painter: MyPainter(
+                    textColor,
+                    backgroundColor,
+                    text,
+                    progress,
+                    fontSize,
+                    radius,
+                    boardWidth),
+                size: Size(width, height)
+            ),
+          )
         ]
     );
-  }
-
-}
-
-class _RatingBarClipper extends CustomClipper<Rect> {
-  final double width;
-
-  _RatingBarClipper({this.width}) : assert(width != null);
-
-  @override
-  Rect getClip(Size size) {
-    return new Rect.fromLTRB(0.0, 0.0, width, size.height);
-  }
-
-  @override
-  bool shouldReclip(_RatingBarClipper oldClipper) {
-    return width != oldClipper.width;
   }
 }
 
@@ -76,19 +63,32 @@ class MyPainter extends CustomPainter {
   var text;
   var progress;
   var textSize;
+  var radius;
+  var boardWidth;
 
   MyPainter(this.backgroundColor, this.textColor, this.text, this.progress,
-      this.textSize);
+      this.textSize,
+      this.radius,
+      this.boardWidth);
 
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
+    //绘制边
+    RRect rect = RRect.fromLTRBR(
+        0.0, 0.0, size.width, size.height, Radius.circular(radius));
     Paint paint = Paint()
+      ..strokeWidth = boardWidth
       ..color = backgroundColor
-      ..style = PaintingStyle.fill;
-    Rect rect = Rect.fromLTRB(
-        0.0, 0.0, size.width, size.height);
-    canvas.drawRect(rect, paint);
+      ..style = PaintingStyle.stroke;
+    canvas.drawRRect(rect, paint);
+    //裁剪已下载部分
+    Rect clipRect = Rect.fromLTRB(
+        0.0, 0.0, size.width * progress, size.height);
+    canvas.clipRect(clipRect);
+    //绘制已下载部分背景与文字
+    paint.style = PaintingStyle.fill;
+    canvas.drawRRect(rect, paint);
+    //绘制底部文字
     TextSpan sp = TextSpan(
         style: TextStyle(fontSize: textSize, color: textColor), text: text);
     TextPainter tp = TextPainter(text: sp,
@@ -97,14 +97,10 @@ class MyPainter extends CustomPainter {
     tp.layout();
     tp.paint(canvas,
         Offset((size.width - tp.width) / 2, (size.height - tp.height) / 2));
-    Rect clipRect = Rect.fromLTRB(
-        20.0, 20.0, 50.0, 50.0);
-//    canvas.clipRect(clipRect, clipOp: ClipOp.difference);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
     return true;
   }
 
